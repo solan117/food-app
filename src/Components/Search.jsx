c516b8b27f690e92477eadc64c169785f9c19bcc
Karan Solanki <20240526+solan117@users.noreply.github.com>
2025-04-18 12:06:25 -0400

    useEffect Hook In React

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    function demo() {
+      console.log("Demo function execute!");
+    }
+    demo();
+  }, [query]);
 
   return (
     <div>
