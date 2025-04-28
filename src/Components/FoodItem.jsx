b5049db4a1740c48a0c4c78caae922c45409e75c
Karan Solanki <20240526+solan117@users.noreply.github.com>
2025-04-27 20:38:58 -0400

    Separated api key in env file. Deleted unnecessary code. Added null checker.

--- src/Components/FoodItem.jsx
+++ src/Components/FoodItem.jsx
@@ -1,37 +1,9 @@
-// import styles from "../CSS/fooditem.module.css";
-//
-// export default function FoodItem({ food }) {
-//   async function fetchFoodDetails(id) {
-//     const res = await fetch(
-//       `https://api.spoonacular.com/recipes/${id}/information?apiKey=e3299e71a50149968f1cdbae1319d789`,
-//     );
-//     const data = await res.json();
-//     console.log(data.instructions);
-//   }
-//
-//   return (
-//     <div className={styles.card}>
-//       <img className={styles.image} src={food.image} alt={food.title} />
-//       <h1 className={styles.title}>{food.title}</h1>
-//       <button
-//         className={styles.button}
-//         onClick={() => {
-//           console.log(food.id);
-//           fetchFoodDetails(food.id);
-//         }}
-//       >
-//         View Recipe
-//       </button>
-//     </div>
-//   );
-// }
-
 import { useState } from "react";
 import styles from "../CSS/fooditem.module.css";
 
 export default function FoodItem({ food, setFoodId }) {
-  const [recipe, setRecipe] = useState(null);
-  const [showRecipe, setShowRecipe] = useState(false);
+  const [recipe] = useState(null);
+  const [showRecipe] = useState(false);
 
   return (
     <div className={styles.card}>
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+  BASE_URL: "https://api.spoonacular.com/recipes",
+  API_KEY: import.meta.env.VITE_SPOONACULAR_API_KEY,
+};
